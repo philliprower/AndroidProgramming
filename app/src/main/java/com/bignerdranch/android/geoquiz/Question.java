16c692619592b57a901cc30eeb1c3ea82fed5fef
club14 <csaa.mobile.dev@gmail.com>
2015-12-21 10:43:32 -0700

    Initial Chapter 1 commit.

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,30 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by club14 on 12/21/15.
+ */
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+}
